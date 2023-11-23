debd6c81e2436581ff0170a4bdc65db6b9ba2fb3
eukolos <eminaksoy35@gmail.com>
2023-11-23 13:55:07 +0300

    first day

---
+++ src/day1/deneme.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+const name = "John";
+const surname = "Doe";
+const age = 40;
+
+console.log(name, surname, age);    //* John Doe 40
---
+++ src/day1/variable.js
@@ -0,0 +1,20 @@
+// Declaring different variables of different data types
+
+let firstName = 'Emin'    // first name of a person
+let lastName = 'Aksoy'      // last name of a person
+let country = 'Turkey'       // country
+let city = 'İstanbul'         // capital city
+let age = 100                 // age in years
+let isMarried = true
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Asabeneh',     //name of a person
+  job = 'teacher',
+  live = 'Finland'
